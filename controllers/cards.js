da12f1beee4d01e2fbc410f9a67d49bbf1eb5c0e
Константин <Kostya.Okhlynin@gmail.com>
2020-11-03 20:06:30 +0300

    Исправил замечания

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,34 +1,24 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-multiple-empty-lines */
-
 const Card = require('../models/card');
-
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((data) => {
       res.status(200).send(data);
     })
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => res.status(404).send({ message: `Произошла ошибка ${err}` }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
     .then((data) => {
-      if (!data) {
-        res.status(404).send({ message: 'Такой карточки нет!' });
-        return;
-      }
       res.status(200).send(data);
     })
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => res.status(404).send({ message: `Такой карточки нет! ${err}` }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((user) => res.status(200).send({ user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => res.status(404).send({ message: `Ошибка,карточка не была создана ${err}` }));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,35 +1,24 @@
-/* eslint-disable no-undef */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-multiple-empty-lines */
-
 const User = require('../models/user');
-
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((data) => {
       res.status(200).send(data);
     })
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => res.status(404).send({ message: `Произошла ошибка ${err}` }));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.id)
     .then((data) => {
-      if (!data) {
-        res.status(404).send({ message: 'Пользователь с таким id не найдён' });
-        return;
-      }
       res.status(200).send(data);
     })
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => res.status(404).send({ message: `Пользователь с таким id не найдён ${err}` }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(200).send({ user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => res.status(404).send({ message: `Ошибка,пользователь не создан ${err}` }));
 };
