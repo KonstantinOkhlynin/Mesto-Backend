faa8e5c6c49de7f2e94fff0bbaa18a795477825a
Константин <Kostya.Okhlynin@gmail.com>
2020-11-04 15:34:33 +0300

    Исправил замечания

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,20 +5,28 @@ module.exports.getCards = (req, res) => {
     .then((data) => {
       res.status(200).send(data);
     })
-    .catch((err) => res.status(404).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => res.status(400).send({ message: `Произошла ошибка ${err}` }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
     .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: 'Такой карточки нет!' });
+      }
       res.status(200).send(data);
     })
-    .catch((err) => res.status(404).send({ message: `Такой карточки нет! ${err}` }));
+    .catch((err) => res.status(400).send({ message: `Неправильный id ${err}` }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((user) => res.status(200).send({ user }))
-    .catch((err) => res.status(404).send({ message: `Ошибка,карточка не была создана ${err}` }));
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Проверьте правильность данных' });
+      }
+      res.status(200).send({ user });
+    })
+    .catch((err) => res.status(400).send({ message: `Ошибка,карточка не была создана ${err}` }));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -11,14 +11,22 @@ module.exports.getUsers = (req, res) => {
 module.exports.getUser = (req, res) => {
   User.findById(req.params.id)
     .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: 'Пользователь с таким id не найдён' });
+      }
       res.status(200).send(data);
     })
-    .catch((err) => res.status(404).send({ message: `Пользователь с таким id не найдён ${err}` }));
+    .catch((err) => res.status(400).send({ message: `Неправильный id ${err}` }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.status(200).send({ user }))
-    .catch((err) => res.status(404).send({ message: `Ошибка,пользователь не создан ${err}` }));
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Проверьте правильность данных' });
+      }
+      res.status(200).send({ user });
+    })
+    .catch((err) => res.status(400).send({ message: `Ошибка,пользователь не создан ${err}` }));
 };
